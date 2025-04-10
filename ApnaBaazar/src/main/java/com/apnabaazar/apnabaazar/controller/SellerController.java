558143dbe50de7ed2edd453e4dc64438047689a5
akshat-ttn <akshat.srivastava@tothenew.com>
2025-04-11 00:42:46 +0530

    Added endpoints for upload profile images

--- ApnaBaazar/src/main/java/com/apnabaazar/apnabaazar/controller/SellerController.java
+++ ApnaBaazar/src/main/java/com/apnabaazar/apnabaazar/controller/SellerController.java
@@ -1,18 +1,26 @@
 package com.apnabaazar.apnabaazar.controller;
 
+import com.apnabaazar.apnabaazar.config.UserPrincipal;
 import com.apnabaazar.apnabaazar.model.dto.SellerProfileDTO;
+import com.apnabaazar.apnabaazar.service.S3Service;
 import com.apnabaazar.apnabaazar.service.SellerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/seller")
 public class SellerController {
 
     private final SellerService sellerService;
+    private final S3Service s3Service;
 
     @GetMapping("/test")
     public String testCustomer(){
@@ -20,7 +28,22 @@ public class SellerController {
     }
 
     @GetMapping("/profile")
-    public ResponseEntity<SellerProfileDTO>  getProfile(){
-        return sellerService.getSellerProfile();
+    public ResponseEntity<SellerProfileDTO>  getSellerProfile(@AuthenticationPrincipal UserPrincipal userPrincipal){
+        return sellerService.getSellerProfile(userPrincipal);
+    }
+
+    @PostMapping("/upload/profile-image")
+    public ResponseEntity<String> uploadProfileImage(@RequestParam MultipartFile file, @AuthenticationPrincipal UserPrincipal userPrincipal) throws IOException {
+        String key = s3Service.uploadSellerProfileImage(userPrincipal.getUsername(),file);
+        return ResponseEntity.ok("Image uploaded at key : "+key);
+    }
+
+    @GetMapping("/profile-image")
+    public ResponseEntity<byte[]> downloadSellerImage(@RequestParam String extension,
+                                                      @AuthenticationPrincipal UserDetails user) {
+        byte[] image = s3Service.downloadSellerProfileImage(user.getUsername(), extension);
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_JPEG)
+                .body(image);
     }
 }
